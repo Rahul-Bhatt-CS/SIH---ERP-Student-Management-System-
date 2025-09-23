bc3321159b3c253891f014daea9b166fe0137a6b
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-09-23 20:19:08 +0530

    Initial Commit

---
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/SihErpSystemApplication.java
@@ -0,0 +1,13 @@
+package com.Student_Management_System.SIH_ERP_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SihErpSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SihErpSystemApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/test/java/com/Student_Management_System/SIH_ERP_System/SihErpSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Student_Management_System.SIH_ERP_System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SihErpSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
