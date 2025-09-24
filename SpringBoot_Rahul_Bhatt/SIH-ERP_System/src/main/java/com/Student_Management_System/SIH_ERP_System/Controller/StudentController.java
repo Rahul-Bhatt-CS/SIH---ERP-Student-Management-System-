9bef29104458d77deca003e12e8006dc92638d75
akshay <aj266670@gmail.com>
2025-09-24 21:32:14 +0530

    kkuch nhi 3

--- SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Controller/StudentController.java
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Controller/StudentController.java
@@ -2,7 +2,6 @@ package com.Student_Management_System.SIH_ERP_System.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//ANish teri maa ki chut
 @RestController
 public class StudentController {
     @GetMapping("/")
