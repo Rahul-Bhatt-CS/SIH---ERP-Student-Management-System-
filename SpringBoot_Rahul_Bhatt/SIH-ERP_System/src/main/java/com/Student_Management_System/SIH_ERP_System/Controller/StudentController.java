0a67aa6dac8a9e5d006333c54d572af4f367e01b
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-09-25 21:11:00 +0530

    register hostel

--- SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Controller/StudentController.java
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Controller/StudentController.java
@@ -1,11 +1,26 @@
 package com.Student_Management_System.SIH_ERP_System.Controller;
 
+import com.Student_Management_System.SIH_ERP_System.Entities.HostelDetails;
+import com.Student_Management_System.SIH_ERP_System.Entities.Student_Entity;
+import com.Student_Management_System.SIH_ERP_System.Services.AuthRepo_StudentService;
+import com.Student_Management_System.SIH_ERP_System.Services.DataRepo_HostelDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StudentController {
-    @GetMapping("/")
-    public String hello(){
-        return "Hello!";
+    @Autowired
+    AuthRepo_StudentService studentService;
+    @Autowired
+    DataRepo_HostelDetailsService hostelDetailsService;
+
+    @PostMapping("/student/hostel")
+    public ResponseEntity<?> registerHostel(@RequestBody HostelDetails details){
+        Student_Entity student = studentService.fetchStudent(details.getStudentid());
+        details.setStudent(student);
+        return hostelDetailsService.registerHostel(details);
     }
 }
--- SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Services/DataRepo_HostelDetailsService.java
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Services/DataRepo_HostelDetailsService.java
@@ -1,11 +1,17 @@
 package com.Student_Management_System.SIH_ERP_System.Services;
 
+import com.Student_Management_System.SIH_ERP_System.Entities.HostelDetails;
 import com.Student_Management_System.SIH_ERP_System.Repositories.DataRepo_HostelDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataRepo_HostelDetailsService {
     @Autowired
     DataRepo_HostelDetails repo_hostelDetails;
+
+    public ResponseEntity<?> registerHostel(HostelDetails details){
+        return ResponseEntity.ok(repo_hostelDetails.save(details));
+    }
 }
