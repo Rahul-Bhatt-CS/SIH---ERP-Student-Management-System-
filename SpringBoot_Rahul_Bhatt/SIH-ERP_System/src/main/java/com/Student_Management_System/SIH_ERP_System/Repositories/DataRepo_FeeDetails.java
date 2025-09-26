649d7c338df88334a3b8bc00e747bd5c47d9e403
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-09-26 16:44:52 +0530

    fee detail repo

--- SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Repositories/DataRepo_FeeDetails.java
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Repositories/DataRepo_FeeDetails.java
@@ -4,7 +4,9 @@ import com.Student_Management_System.SIH_ERP_System.Entities.FeeDetails;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DataRepo_FeeDetails extends JpaRepository<FeeDetails, String> {
-    FeeDetails findBySId(String id);
+    List<FeeDetails> findBySId(String id);
 }
