02952a0edc537f0e48ffb0aa42a6b4d20ba41866
Rahul Bhatt <rbhatt.cs@gmail.com>
2025-09-26 16:45:24 +0530

    fee detail repo

--- SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Services/DataRepo_FeeDetailsService.java
+++ SpringBoot_Rahul_Bhatt/SIH-ERP_System/src/main/java/com/Student_Management_System/SIH_ERP_System/Services/DataRepo_FeeDetailsService.java
@@ -6,12 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class DataRepo_FeeDetailsService {
     @Autowired
     DataRepo_FeeDetails repoFeeDetails;
 
-    public FeeDetails fetchFee(String id){return repoFeeDetails.findBySId(id);}
+    public List<FeeDetails> fetchFee(String id){return repoFeeDetails.findBySId(id);}
 
 
     public ResponseEntity<?> semesterFee(FeeDetails details){
